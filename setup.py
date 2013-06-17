de4cc89f4ed7663b61efe3ef2dfe1b3749871345
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-17 19:41:13 -0400

    First cut on the package distribution file.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""pyxda - X-ray Data Analysis Library
+"""
+
+from distutils.core import setup
+
+# define distribution
+setup(
+        name = "pyxda",
+        version = "1.0",
+        packages = ["pyxda", "pyxda.tests"],
+        package_data = {"pyxda" : ['tests/testdata/*']},
+        author = 'Simon J.L. Billinge',
+        author_email = 'sb2896@columbia.edu',
+        maintainer = 'Pavol Juhas',
+        maintainer_email = 'pavol.juhas@gmail.com',
+        # url = '',
+        # download_url = 'FIXME to github URL',
+        description = "X-ray Data Analysis Library",
+        license = 'BSD',
+        classifiers = [
+            # List of possible values at
+            # http://pypi.python.org/pypi?:action=list_classifiers
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Physics',
+        ],
+)
