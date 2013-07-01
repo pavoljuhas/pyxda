7d7094c757669f6c4b65d72ebb5498e36ebd571b
Pavol Juhas <pavol.juhas@gmail.com>
2013-07-01 14:48:21 -0400

    Subpackage for the viewer application.

---
+++ pyxda/rawviewer/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+##############################################################################
+#
+# pyxda             X-ray Data Analysis Library
+#
+# File coded by:    FIXME
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+"""Raw viewer application for 2D images from X-ray detectors.
+"""
